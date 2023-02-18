3df426e18de2a4bff0896bc3077ade8f951a0bb2
Ana Coronel <a9nna.coronel@gmail.com>
2023-02-18 14:59:49 +0100

    Link the createDatabase function with an url database

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import "./loadEnvironment.js";
+import connectDatabase from "./database/connectDatabase.js";
 import { startServer } from "./server/startServer.js";
 
 const port = process.env.PORT ?? 4000;
+const mongoUrl = process.env.MONGODB_CONNECTION_URL!;
 
-startServer(+port);
+await startServer(+port);
+await connectDatabase(mongoUrl);
