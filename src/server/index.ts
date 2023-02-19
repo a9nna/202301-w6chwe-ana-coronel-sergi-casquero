fa273db2a01f175cdcbcd69482bd2ae33307510b
Ana Coronel <a9nna.coronel@gmail.com>
2023-02-19 23:08:41 +0100

    Set up cors

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,11 +2,15 @@ import express from "express";
 import morgan from "morgan";
 import { generalError, notFoundError } from "./middleware/errorMiddlewares.js";
 import robotsRouter from "./routers/robotsRouters.js";
+import cors from "cors";
 
 const app = express();
 
+app.use(cors());
+
 app.use(morgan("dev"));
 
+app.use(express.json());
 app.use("/robots", robotsRouter);
 
 app.use(notFoundError);
