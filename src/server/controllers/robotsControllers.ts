0abab0c2d709f45e7ed22e888b68505b525e25e1
Sergi Casquero <sergicasquero@gmail.com>
2023-02-20 19:23:10 +0100

    Add createRobot endpoint and controller

--- src/server/controllers/robotsControllers.ts
+++ src/server/controllers/robotsControllers.ts
@@ -1,6 +1,7 @@
 import { type NextFunction, type Request, type Response } from "express";
 import Robot from "../../database/models/Robot.js";
 import CustomError from "../CustomError.js";
+import { type RobotStructure } from "../types.js";
 
 export const getRobots = async (
   req: Request,
@@ -33,5 +34,29 @@ export const getRobotById = async (
     res.status(200).json({ robot });
   } catch (error) {
     next(new CustomError(error.message, 500, "Couldn't retrieve the robot"));
+  }
+};
+
+export const createRobot = async (
+  req: Request<
+    Record<string, unknown>,
+    Record<string, unknown>,
+    RobotStructure
+  >,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const newRobot = req.body;
+    await Robot.create(newRobot);
+
+    res.status(201).json({ newRobot });
+  } catch (error) {
+    const customError = new CustomError(
+      error.message,
+      400,
+      "Couldn't create your new robot"
+    );
+    next(customError);
   }
 };
